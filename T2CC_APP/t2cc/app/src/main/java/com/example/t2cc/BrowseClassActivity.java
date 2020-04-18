5ab9d45cd1e564aa444c9a25e2f35f5b04a71fb3
avan989 <avan989@yahoo.com>
2020-04-18 16:20:38 -0400

    add in browse class view

---
+++ T2CC_APP/t2cc/app/src/main/java/com/example/t2cc/BrowseClassActivity.java
@@ -0,0 +1,14 @@
+package com.example.t2cc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BrowseClassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browseclass);
+    }
+}
--- T2CC_APP/t2cc/app/src/main/java/com/example/t2cc/HomeActivity.java
+++ T2CC_APP/t2cc/app/src/main/java/com/example/t2cc/HomeActivity.java
@@ -2,13 +2,35 @@ package com.example.t2cc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home);
+
+    // Buttons
+    findViewById(R.id.browseClassesButton).setOnClickListener(this);
+
   }
+
+  @Override
+  public void onClick(View v) {
+    int i = v.getId();
+
+    if (i == R.id.browseClassesButton) {
+      changeToBrowseActivity();
+
+    }
+  }
+
+  private void changeToBrowseActivity() {
+    Intent intent = new Intent(HomeActivity.this, BrowseClassActivity.class);
+    startActivity(intent);
+  }
+
 }
